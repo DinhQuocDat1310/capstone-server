fedec85a334688cf3fd988d2fcd3002ce1fffac9
dinh dat <dinhquocdat1310@gmail.com>
2022-10-23 02:28:36 +0700

    create faqs

---
+++ src/faq/controller.ts
@@ -0,0 +1,72 @@
+import { UserStatus, Role } from '@prisma/client';
+import { FAQDto } from './dto';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/guard/roles.guard';
+import { StatusGuard } from 'src/guard/userStatus.guard';
+import { FaqService } from './service';
+import { Roles, Status } from 'src/guard/decorators';
+
+@Controller('faq')
+@UseGuards(JwtAuthGuard, RolesGuard, StatusGuard)
+@ApiBearerAuth('access-token')
+@ApiTags('FAQs')
+export class FaqController {
+  constructor(private readonly faqService: FaqService) {}
+
+  @ApiBody({ type: FAQDto })
+  @ApiOperation({ summary: 'Create a FAQ' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiCreatedResponse({ description: 'Created' })
+  @Status(UserStatus.VERIFIED)
+  @Roles(Role.ADMIN)
+  @Post('/create')
+  async createManager(@Body() dto: FAQDto) {
+    return await this.faqService.createFAQ(dto);
+  }
+
+  @ApiOperation({ summary: 'View list FAQs' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @Status(UserStatus.VERIFIED)
+  @Roles(Role.ADMIN)
+  @Get('/list')
+  async viewListFAQ() {
+    return await this.faqService.viewListFAQs();
+  }
+
+  @ApiOperation({ summary: 'Enable a FAQ by FAQId' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @Status(UserStatus.VERIFIED)
+  @Roles(Role.ADMIN)
+  @Get('/enable/:id')
+  async enableFaq(@Param('id') id: string) {
+    return await this.faqService.enableFaq(id);
+  }
+
+  @ApiOperation({ summary: 'Disable a FAQ by FAQId' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @Status(UserStatus.VERIFIED)
+  @Roles(Role.ADMIN)
+  @Get('/disable/:id')
+  async disableFaq(@Param('id') id: string) {
+    return await this.faqService.disableFaq(id);
+  }
+}
---
+++ src/faq/service.ts
@@ -0,0 +1,55 @@
+import { FAQDto } from './dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/service';
+
+@Injectable()
+export class FaqService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createFAQ(dto: FAQDto) {
+    await this.prisma.fAQs.create({
+      data: {
+        question: dto.question,
+        answer: dto.answer,
+      },
+    });
+    return `Created`;
+  }
+
+  async viewListFAQs() {
+    return await this.prisma.fAQs.findMany({});
+  }
+
+  async enableFaq(id: string) {
+    await this.checkIdFaq(id);
+    await this.prisma.fAQs.update({
+      where: {
+        id,
+      },
+      data: {
+        isActive: true,
+      },
+    });
+    return `Enable Faq`;
+  }
+
+  async disableFaq(id: string) {
+    await this.checkIdFaq(id);
+    await this.prisma.fAQs.update({
+      where: {
+        id,
+      },
+      data: {
+        isActive: false,
+      },
+    });
+    return `Disable Faq`;
+  }
+
+  async checkIdFaq(id: string) {
+    const checkIdFaq = await this.prisma.fAQs.findFirst({
+      where: { id },
+    });
+    if (!checkIdFaq) throw new BadRequestException('Not found faqID');
+  }
+}
