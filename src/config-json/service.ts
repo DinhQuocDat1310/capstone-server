c083c82b5d2cd963d018d0b6c79c465df5efb278
dinh dat <dinhquocdat1310@gmail.com>
2022-11-01 01:00:48 +0700

    update unique duration

--- src/config-json/service.ts
+++ src/config-json/service.ts
@@ -4,9 +4,31 @@ import * as fs from 'fs';
 
 @Injectable()
 export class ConfigJsonService {
-  listDataConfig = () =>
-    JSON.parse(fs.readFileSync('./dataConfig.json', 'utf-8'));
+  listDataConfig = () => {
+    return JSON.parse(fs.readFileSync('./dataConfig.json', 'utf-8'));
+  };
 
-  saveJsonDataConfig = (dto: VariableConfig) =>
-    fs.writeFileSync('./dataConfig.json', JSON.stringify(dto));
+  saveJsonDataConfig = (dto: VariableConfig) => {
+    const listDataDuration = this.listDataConfig().duration;
+    const listDtoDuration = dto.duration;
+    const uniqueListDtoDuration = Array.from(new Set(listDtoDuration));
+    const duplicateDuration = listDataDuration.filter(
+      (data: string) => uniqueListDtoDuration.indexOf(data) !== -1,
+    );
+    const newDuration = listDataDuration
+      .filter((data: string) => !uniqueListDtoDuration.includes(data))
+      .concat(
+        uniqueListDtoDuration.filter(
+          (data) => !listDataDuration.includes(data),
+        ),
+      );
+    const data = {
+      duration: [...duplicateDuration, ...newDuration],
+      minimumKmDrive: dto.minimumKmDrive,
+      gapOpenRegisterForm: dto.gapOpenRegisterForm,
+      gapPaymentDeposit: dto.gapPaymentDeposit,
+      gapWrapping: dto.gapWrapping,
+    };
+    return fs.writeFileSync('./dataConfig.json', JSON.stringify(data));
+  };
 }
