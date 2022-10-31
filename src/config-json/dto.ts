12058638eb818f3dea7c74655a1ab09d6d4c2bf1
dinh dat <dinhquocdat1310@gmail.com>
2022-10-31 16:08:10 +0700

    create json file and api for admin config data

---
+++ src/config-json/dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsString } from 'class-validator';
+
+export class VariableConfig {
+  @IsArray()
+  @ApiProperty({
+    type: Array,
+    description: 'Duration',
+    default: ['30', '60', '90'],
+  })
+  duration: string[];
+
+  @IsString()
+  @ApiProperty({ type: String, description: 'MinimumKmDrive', default: '20' })
+  minimumKmDrive: string;
+
+  @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'GapOpenRegisterForm',
+    default: '5',
+  })
+  gapOpenRegisterForm: string;
+
+  @IsString()
+  @ApiProperty({ type: String, description: 'GapPaymentDeposit', default: '5' })
+  gapPaymentDeposit: string;
+
+  @IsString()
+  @ApiProperty({ type: String, description: 'GapWrapping', default: '5' })
+  gapWrapping: string;
+}
