3c36fcdb6d9a5d7ce22959dcb4a6530a74023846
Balleys1900 <leminhtai325@gmail.com>
2022-11-07 15:18:04 +0000

    Feat/integrate paypal payment

--- src/payment/controller.ts
+++ src/payment/controller.ts
@@ -1,5 +1,11 @@
-import { All, Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { All, Body, Controller, Param, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PaymentService } from './service';
 
 @Controller('payment')
@@ -7,11 +13,16 @@ import { PaymentService } from './service';
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
+  @ApiOperation({ summary: 'Create Transaction Paypal' })
+  @ApiBody({ type: String })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiCreatedResponse({ description: 'Created' })
   @Post('/orders')
   async createTransaction(@Body() dto: { contractId: string }) {
     return await this.paymentService.createOrder(dto.contractId);
   }
 
+  @ApiOperation({ summary: 'Checkout transaction' })
   @Post('/orders/:id/capture')
   async captureTransaction(@Param('id') orderId: string) {
     return await this.paymentService.capturePayment(orderId);
